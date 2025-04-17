8a6c1e87a9e3f179975a995448adf464ffff6b09
fahey-bnl <kfahey@bnl.gov>
2025-04-17 15:49:04 -0400

    IOC Tweaks

--- ioc-zcu104Test/iocBoot/iocdemo/st.cmd
+++ ioc-zcu104Test/iocBoot/iocdemo/st.cmd
@@ -1,22 +1,24 @@
-#!../../bin/linux-x86_64/myioc
+#!../../bin/linux-x86_64/zcu104Test
 
 #- SPDX-FileCopyrightText: 2005 Argonne National Laboratory
 #-
 #- SPDX-License-Identifier: EPICS
 
-#- You may have to change myioc to something else
+#- You may have to change zcu104Test to something else
 #- everywhere it appears in this file
 
 #< envPaths
 
 ## Register all support components
-dbLoadDatabase "../../dbd/myioc.dbd"
-myioc_registerRecordDeviceDriver(pdbbase) 
+dbLoadDatabase "../../dbd/zcu104Test.dbd"
+zcu104Test_registerRecordDeviceDriver(pdbbase) 
 
+## Load dev1: device input @ 192.168.1.10:7 with receive timeout turned off
 createPSC("dev1", "192.168.1.10", 7, 0)
 setPSCSendBlockSize("dev1", 0, 8)
 setPSCSendBlockSize("dev1", 4, 8)
 
+## Load dev2: device output @ 192.168.1.10:600 with receive timeout turned on
 createPSC("dev2", "192.168.1.10", 600, 1)
 
 ## Load record instances
